508871f038a0cf929a460ab53bd93cdd70457729
Thomas Volkaert <37460087+Adelixxe@users.noreply.github.com>
2020-04-07 19:53:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,14 +14,24 @@ bot.on("debug", (e) => console.info(e));
 bot.on('ready', function() {
     console.log(" McEDT")
 });
-
+fonction
 bot.on('ready', () =>{
     bot.user.setPresence({ game: {name: " le cours @Adelixxe", type: 2}});
 });
+function actualisation(){
+    var date = new Date();
+    var jour = date.getDay();
+    var heure = date.getHours();
+    var minutes = date.getMinutes();
+};
+while(1){
+    actualisation
+    await sleep(5000);
+}
 bot.on('message', message => {
     if(jour === 2){
         if(heure === 17){
-            if(minutes === 43){
+            if(minutes === 56){
                 Discord.client.channels.get('697131247201288253').send('Il est 19h43(test)')
                 }
             }
